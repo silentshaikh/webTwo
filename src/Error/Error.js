4f2135bc22aa1d1d507024631b41cc18633f3403
silentshaikh <sheikhabdulmoiz283@gmail.com>
2023-11-20 14:41:35 +0500

    update style

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Error() {
   return (
-    <section className='h-screen flex justify-center items-center flex-col'>
+    <section className='errror'>
         <h1 className='text-3xl'>404</h1>
         <h1 className='text-3xl text-teal-300'>Error</h1>
         <button className='bg-teal-300 text-white px-7 py-3 rounded-full'>Go Back</button>
